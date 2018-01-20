2f9a7ec1da174d04aaefc8ca5350706875794e30
Xiaobaibai <824395314@qq.com>
2018-01-21 04:09:53 +0800

    去掉没卵用的cvar

--- FuckWorld/Version.cpp
+++ FuckWorld/Version.cpp
@@ -5,18 +5,8 @@
 
 char g_szVersion[] = __DATE__;
 
-cvar_t *g_pcvarVersion;
-
-void Version_Response()
-{
-}
-
 void Version_Init()
 {
-	g_pcvarVersion = gEngfuncs.pfnRegisterVariable("tz_version", g_szVersion, 0);
-
-	gEngfuncs.pfnAddCommand("tz_versionsync", Version_Response);
-
 	gEngfuncs.PlayerInfo_SetValueForKey("tz_version", g_szVersion);
 	gEngfuncs.pfnClientCmd(va("setinfo \"tz_version\" \"%s\"", g_szVersion));
 }
